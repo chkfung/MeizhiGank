f39b8500f6597230ccce25acf212b0519fd72635
Lam Fung <chkfung93@gmail.com>
2016-09-01 15:50:28 +0800

    Update License

--- app/src/main/java/me/chkfung/meizhigank/Dagger/Module/GankPresenterModule.java
+++ app/src/main/java/me/chkfung/meizhigank/Dagger/Module/GankPresenterModule.java
@@ -1,8 +1,28 @@
+/*
+ * Meizhi & Gank.io
+ * Copyright (C) 2016 ChkFung
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package me.chkfung.meizhigank.Dagger.Module;
 
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 
 import dagger.Module;
 import dagger.Provides;
@@ -39,7 +59,7 @@ public class GankPresenterModule {
     CustomTabsIntent.Builder provideCustomTabIntent(GankContract.View view) {
         Context mContext = view.getContext();
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        builder.setToolbarColor(mContext.getResources().getColor(R.color.colorPrimary))
+        builder.setToolbarColor(ContextCompat.getColor(mContext, R.color.colorPrimary))
                 .setShowTitle(true);
 
         builder.setStartAnimations(mContext, R.anim.right_in, R.anim.right_out);
